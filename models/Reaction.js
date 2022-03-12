c408f1c6a43e662cf0c9725d19805f145b8cc273
besparza <esparzabrian90@gmail.com>
2022-03-12 11:23:45 -0800

    added routes and controllers

---
+++ models/Reaction.js
@@ -0,0 +1,27 @@
+const ReactionSchema = new Schema(
+    {
+      reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+      },
+      reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+      },
+      username: {
+        type: String,
+        required: true
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+      }
+    },
+    {
+      toJSON: {
+        getters: true
+      }
+    }
+  );
